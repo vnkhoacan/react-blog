142f1584d444e8f4ccf6eef759d6009b55b4be09
Khoa Vo <vnkhoacan@gmail.com>
2023-05-19 11:01:21 +0700

    update code

---
+++ react-mui-blog/src/services/post.service.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import authHeader from "./auth-header";
+
+const API_URL = "http://127.0.0.1:8000/api/posts"
+
+const getAll = () => {
+    return axios.get(API_URL);
+}
+
+const getOne = (id) => {
+    return axios.get(API_URL + "/" + id)
+}
+
+const getMy = () => {
+    return axios.get(API_URL + "/my-post", { headers: authHeader()})
+}
+
+const create = (data) => {
+    return axios.post(API_URL, data, { headers: authHeader()})
+}
+
+const update = (data, id) => {
+    return axios.put(API_URL + "/" + id, data, { headers: authHeader()})
+}
+
+const destroy = (id) => {
+    return axios.delete(API_URL + "/" + id, { headers: authHeader()})
+}
+
+const PostService = {
+    getAll,
+    getOne,
+    getMy,
+    create,
+    update,
+    destroy,
+}
+
+export default PostService
